f27bf0ad27239c232b4a64dc8799e411089c18cc
Lindsey Jones <lindseyelizabethjones@gmail.com>
2012-02-02 12:43:58 -0700

    Create a class

--- ruby_intro.rb
+++ ruby_intro.rb
@@ -6,3 +6,44 @@
 class RubyIntro
 end
 
+
+# current_date
+
+class RubyIntro
+time = Time.new
+end
+
+
+# pragmatically (don’t type it out) an array with the numbers 1 - 1000
+
+class RubyIntro
+x=(1..1000).to_a
+end
+
+
+# create a method that adds two numbers
+
+class RubyIntro
+def add
+1+1
+end
+end
+
+RubyIntro.new
+rubyintro=RubyIntro.new
+rubyintro.add
+
+
+#set a constant on the class
+
+class RubyIntro
+MY_NAME="Lindsey"
+end
+
+
+#create a method that checks for a word in a string
+
+class RubyIntro
+s="I am learning Ruby"
+s.scan("learning)
+end
